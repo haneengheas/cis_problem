57481b1daac9ee9821a55b0335240a92fcbd165b
haneengheas <haneengheas@gmail.com>
2021-12-16 08:28:11 +0200

    0

--- bin/cis_problem.dart
+++ bin/cis_problem.dart
@@ -4,14 +4,14 @@ import 'dart:io';
 void main(List<String> arguments) {
   print('enter the text');
   String? text = stdin.readLineSync();
-  if(ispoly(text!)) {
+  if(isPalindrome(text!)) {
     print('true');
   } else {
     print('false');
   }
 
 }
-bool ispoly (String text){
+bool isPalindrome (String text){
   int start =0;
   int end = text.length-1 ;
    while(start<end){
